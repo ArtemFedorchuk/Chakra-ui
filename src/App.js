dfc1709fd225fff3c58d4ab823511e6935d47048
Artem <fedorchuk.zver@gmail.com>
2020-11-16 23:00:47 +0200

    add ESlint and Pretier

--- src/App.js
+++ src/App.js
@@ -7,32 +7,31 @@ import LogIn from './organisms/LogIn';
 import { ThemeContext } from './contexts/';
 
 function App() {
-  const auth = localStorage.getItem('auth');
+	const auth = localStorage.getItem('auth');
 
-  const protectedRoute = (Component) => {
-    if (auth === 'true') {
-      return Component
-    }
-    else {
-     return <Redirect to="/" />
-    }
-  };
+	const protectedRoute = (Component) => {
+		if (auth === 'true') {
+			return Component;
+		} else {
+			return <Redirect to='/' />;
+		}
+	};
 
-  return (
-    <div className="App">
-      <ThemeContext>
-        <Switch>
-          <Route exact path="/" render={ () => <LogIn/>} />
-          <Route path="/sign-in" render={ () => <LogIn/>} />
-          <Route path="/forgot-password" render={ () => <h1>ForgotPasswordPage</h1>} />
-          <Route path="/sign-up" render={ () => <h1>SignUpPage</h1>} />
-          <Route path="/reset-password" render={ () => <h1>ResetPasswordPage</h1>} />
-          {protectedRoute(<Route path="/home" render={ () =>  <HomePage/>} />)}
-          <Route render={ () => <ErrorPage />} />
-        </Switch>
-      </ThemeContext>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<ThemeContext>
+				<Switch>
+					<Route exact path='/' render={() => <LogIn />} />
+					<Route path='/sign-in' render={() => <LogIn />} />
+					<Route path='/forgot-password' render={() => <h1>ForgotPasswordPage</h1>} />
+					<Route path='/sign-up' render={() => <h1>SignUpPage</h1>} />
+					<Route path='/reset-password' render={() => <h1>ResetPasswordPage</h1>} />
+					{protectedRoute(<Route path='/home' render={() => <HomePage />} />)}
+					<Route render={() => <ErrorPage />} />
+				</Switch>
+			</ThemeContext>
+		</div>
+	);
 }
 
 export default App;
--- src/DataContext.js
+++ src/DataContext.js
@@ -3,21 +3,20 @@ import React, { createContext, useContext, useState } from 'react';
 const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
+	const [data, setData] = useState({});
 
-  const [ data, setData ] = useState({});
+	const setValues = (values) => {
+		setData((prev) => ({
+			...prev,
+			...values,
+		}));
+	};
 
-  const setValues = (values) => {
-    setData((prev) => ({
-      ...prev,
-      ...values
-    }))
-  };
-
-  return (
-      <DataContext.Provider value={{data, setValues}}>
-        { children }
-      </DataContext.Provider>
-  )
+	return (
+		<DataContext.Provider value={{ data, setValues }}>
+			{children}
+		</DataContext.Provider>
+	);
 };
 
 export const useData = () => useContext(DataContext);
